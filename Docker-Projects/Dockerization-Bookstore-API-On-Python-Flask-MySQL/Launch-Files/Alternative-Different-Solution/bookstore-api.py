70be3ca7a9c3b358a7fb35aa722f9ee88b935a33
Yunus <yunusdelibas@outlook.com>
2022-04-24 21:31:33 +0300

    password in bookstore-api.py and docker-compose.yml is changed...

--- Docker-Projects/Dockerization-Bookstore-API-On-Python-Flask-MySQL/Launch-Files/Alternative-Different-Solution/bookstore-api.py
+++ Docker-Projects/Dockerization-Bookstore-API-On-Python-Flask-MySQL/Launch-Files/Alternative-Different-Solution/bookstore-api.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 # Configure sqlite database
 app.config['MYSQL_DATABASE_HOST'] = 'database'
 app.config['MYSQL_DATABASE_USER'] = 'yakut'
-app.config['MYSQL_DATABASE_PASSWORD'] = 'Yakut2013'
+app.config['MYSQL_DATABASE_PASSWORD'] = '' # Enter your password here.
 app.config['MYSQL_DATABASE_DB'] = 'bookstore_db'
 app.config['MYSQL_DATABASE_PORT'] = 3306
 mysql = MySQL()
